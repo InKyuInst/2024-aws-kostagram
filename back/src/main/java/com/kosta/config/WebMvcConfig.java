c4a6d16ebf78a2cc527be8d2afbebe0bc4791c78
InKyuInst <148037709+InKyuInst@users.noreply.github.com>
2024-09-28 21:14:16 +0900

    Update WebMvcConfig.java

--- back/src/main/java/com/kosta/config/WebMvcConfig.java
+++ back/src/main/java/com/kosta/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry
 			.addMapping("/**")
-			.allowedOrigins("http://192.168.135.128")
+			.allowedOrigins("http://3.35.135.49")
 			.allowedMethods("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
 			.allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true); // 자격 증명 허용 (쿠키, 인증 정보)
